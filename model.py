630e2a48c019050c1754b9b14feb9454c75041cf
simonebonato <simone.bonato@fhnw.ch>
2023-11-14 16:52:20 +0100

    simplify model for byol

--- model.py
+++ model.py
@@ -13,24 +13,17 @@ class Model(nn.Module):
             nn.Conv2d(self.input_dims, 32, 3, padding="same"),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(32, 64, 3, padding="same"),
+        )
+        self.fcl = nn.Sequential(
+            nn.Linear(8192, 2048),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.Linear(2048, self.output_dims)
         )
 
-        self.fcl = nn.Sequential(
-            nn.Linear(4096, 2048),
-            nn.ReLU(),
-            nn.Linear(2048, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, self.output_dims)
-        )
-  
         self.relu_pool = nn.Sequential(
             nn.ReLU(),
             nn.MaxPool2d(2, 2)
         )
-
         self.flatten = nn.Flatten()
 
 
@@ -41,4 +34,3 @@ class Model(nn.Module):
         x = self.fcl(x)
 
         return x
-
