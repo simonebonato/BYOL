cee5efa5698f984fa3fdb0bdce39d23e68a79282
simonebonato <simone.bonato@fhnw.ch>
2023-11-29 14:35:12 +0100

    add normalization to augmentations

--- augmentations.py
+++ augmentations.py
@@ -1,6 +1,7 @@
 import numpy as np
-import torchvision.transforms as T
 import torch
+import torchvision.transforms as T
+
 
 class OptionalTransform:
     def __init__(self, transform, transform_params, p):
@@ -13,6 +14,7 @@ class OptionalTransform:
         else:
             return x
 
+
 class Clamp:
     def __init__(self, min, max):
         self.min = min
@@ -20,6 +22,7 @@ class Clamp:
 
     def __call__(self, x):
         return torch.clamp(x, self.min, self.max)
+
 
 class ByolAugmentations:
     def __init__(self, img_shape: tuple):
@@ -31,22 +34,26 @@ class ByolAugmentations:
                 T.ColorJitter,
                 {"brightness": 0.4, "contrast": 0.4, "saturation": 0.4, "hue": 0.1},
                 p=0.8,
-            ), 
+            ),
             T.RandomGrayscale(p=0.2),
         ]
 
-
         self.aug1 = T.Compose(
-            base_transforms + [OptionalTransform(T.GaussianBlur, {"kernel_size": 23, "sigma": (0.1, 2.0)}, p=1.0)]
+            base_transforms
+            + [
+                OptionalTransform(T.GaussianBlur, {"kernel_size": (3, 3), "sigma": (0.1, 2.0)}, p=1.0),
+                T.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
+            ]
         )
 
         self.aug2 = T.Compose(
             base_transforms
             + [
-                OptionalTransform(T.GaussianBlur, {"kernel_size": 23, "sigma": (0.1, 2.0)}, p=0.1),
+                OptionalTransform(T.GaussianBlur, {"kernel_size": (3, 3), "sigma": (0.1, 2.0)}, p=0.1),
                 OptionalTransform(T.RandomSolarize, {"threshold": 0.5}, p=0.2),
+                T.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
             ],
         )
-    
+
     def get_augmentations(self):
         return self.aug1, self.aug2
