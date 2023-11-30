efeefb6f0ca3927f23b6b4ea053db6c8cab1cb54
simonebonato <simone.bonato@fhnw.ch>
2023-11-30 16:51:26 +0100

    update test dataset

--- utils.py
+++ utils.py
@@ -52,16 +52,16 @@ def get_tb_logger(log_dir, extra_str=None):
     return logger
 
 def get_dataloaders(data, val_data, batch_size, test):
-    if test:
-        data = torch.utils.data.Subset(data, range(1000))
-        val_data = torch.utils.data.Subset(val_data, range(1000))
+    if test is not None:
+        data = torch.utils.data.Subset(data, range(min(test, len(data))))
+        val_data = torch.utils.data.Subset(val_data, range(min(test, len(data))))
 
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_data, batch_size=batch_size)
     return loader, val_loader
 
 
-def get_loaders_CIFAR10(batch_size, test=False):
+def get_loaders_CIFAR10(batch_size, test=None):
     data = torchvision.datasets.CIFAR10(
         "./data", download=True, transform=torchvision.transforms.ToTensor(), train=True
     )
@@ -73,7 +73,7 @@ def get_loaders_CIFAR10(batch_size, test=False):
 
     return loader, val_loader
 
-def get_loaders_STL10(batch_size, test=False):
+def get_loaders_STL10(batch_size, test=None):
     data = torchvision.datasets.STL10(
         "./data", download=True, transform=torchvision.transforms.ToTensor(), split="train",
     )
