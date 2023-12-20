148d786db76f0694eb3d239112791bd42fb8662f
simonebonato <simone.bonato@fhnw.ch>
2023-12-20 13:32:01 +0100

    changes

--- pixpro_utils.py
+++ pixpro_utils.py
@@ -1,5 +1,11 @@
 import torch
+from torch.utils.data import DataLoader
 import torch.nn as nn
+from karies.data.dataset import CariesDataset
+from typing import Dict, Tuple
+import cv2
+import numpy as np
+
 
 
 class MaskRCNNModelWrapper(nn.Module):
@@ -20,3 +26,61 @@ class MaskRCNNModelWrapper(nn.Module):
         x = self.avgpool(x)
         x = self.flatten(x)
         return x
+
+
+class PretrainingDataset(CariesDataset):
+
+    def __init__(
+        self,
+        config,
+        mode,
+        only_seg_masks: bool = False,
+        crop_size: int= 112,
+    ):
+        super().__init__(config, mode, only_seg_masks)
+        self.crop_size = crop_size
+        self.config = config
+
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, Dict[str, torch.Tensor]]:
+        """
+        Get item from dataset.
+
+        Args:
+            index (int): index of element
+
+        Returns:
+            Tuple[torch.Tensor, Dict[str, torch.Tensor]]: image tensor and label dict with boxes, labels and masks
+        """
+        label: Dict = self.labels[index]
+        image = cv2.imread(str(label["path"]), cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        if self.config["histogram_eq"]:
+            image = (image[:, :, 0] * 255).astype(np.uint8)
+            image = self.equalize_clahe_image(image)
+            image = image.astype(np.float32) / 255.0
+            image = np.expand_dims(image, -1).repeat(3, -1)
+
+        masks = torch.tensor(np.zeros_like(image))
+        image_tensor, _ = self.apply_augmentations_image_and_masks(image, masks)
+
+        return image_tensor
+    
+    def get_data_loaders(self):
+
+        gen = torch.Generator()
+        if self.config["fix_random_seed"] > -1:
+            gen.manual_seed(self.config["fix_random_seed"])
+
+        # create data loaders
+        SSL_loader = DataLoader(
+            self,
+            batch_size=self.config["batch_size"],
+            shuffle=True,
+            num_workers=self.config["num_workers"],
+            worker_init_fn=self.config["fix_random_seed"] if self.config["fix_random_seed"] > -1 else None,
+            collate_fn=None,
+            generator=gen,
+        )
+        return SSL_loader
+    
